b50cff229a93d4ccf77ca1806c951b90d41f27df
AnoshR <anoshrukadikar@gmail.com>
2026-04-30 00:05:33 +0530

    made changes in deployment.yml && built a new version of app

--- app/app.py
+++ app/app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from DevOps CI/CD Project"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-
 @app.route("/health")
 def health():
     return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
